7a4cde372f8ee72712c91eb35019ed0fb510c12c
Noor Mohammad <noor.jsdivs@gmail.com>
2025-01-20 00:17:30 +0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAKSixEicWLTv5lPxo2CGVNq2wOrbDsBg",
-  authDomain: "test-app-e27fe.firebaseapp.com",
-  projectId: "test-app-e27fe",
-  storageBucket: "test-app-e27fe.appspot.com",
-  messagingSenderId: "285966687345",
-  appId: "1:285966687345:web:2b307394fb94efc481cfd0",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
